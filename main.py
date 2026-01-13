4fa6b2c44663453287461485adaffad58650f752
rhys.bonwick <rhys.bonwick.101@cranfield.ac.uk>
2026-01-13 09:24:52 +0000

    inital classes cerated for the tkinter app such that additnal pages can be added to a single window

---
+++ main.py
@@ -0,0 +1,113 @@
+import tkinter as tk
+from tkinter import ttk
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("LifeFitness App")
+        self.geometry("910x540")
+        self.navbar = None 
+        self.container = tk.Frame(self)
+        self.container.pack(fill="both", expand=True)
+       
+        self.frames = {}
+
+        # Add each page class here
+        # we are using the actual class here and not an instance of the class
+        for Page in (LoginPage, HomePage,overDueBalancePage):
+            # Page() creates a new instance of the current class we are iterating over
+            # by passing in self we can access the methods of this class within every page class (showFrame())
+            frame = Page(self.container, self)
+            #store the instance within the frames property
+            self.frames[Page] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        # Initally show the LoginPage
+        self.show_frame(LoginPage)
+
+    def show_frame(self, page):
+        frame = self.frames[page]
+        # tkraise and classes is the standard for switching between pages
+        frame.tkraise()
+
+    def logout(self,page):
+        self.show_frame(page)
+        # hide navbar
+        self.navbar.pack_forget()
+        self.navbar = None
+
+    def create_navbar(self):
+        if self.navbar:
+            return 
+        
+        self.navbar = tk.Frame(self, bg="#ddd", height=50) 
+        self.navbar.pack(fill="x") 
+
+        tk.Button(self.navbar, text="Home", command=lambda: self.show_frame(HomePage)).pack(side="top",pady="10") 
+        tk.Button(self.navbar, text="Overdue", command=lambda: self.show_frame(overDueBalancePage)).pack(side="top",pady="10") 
+        tk.Button(self.navbar, text="Logout", command=lambda: self.logout(LoginPage)).pack(side="top",pady="10")
+
+
+class LoginPage(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        # instatiate the frame as you would instantiate a normal frame
+        tk.Label(self, text="Login", font=("Arial", 18)).pack(pady=20)
+
+        tk.Label(self, text="Username").pack()
+        self.username = tk.Entry(self)
+        self.username.pack()
+
+        tk.Label(self, text="Password").pack()
+        self.password = tk.Entry(self, show="*")
+        self.password.pack()
+
+        tk.Button(
+            self,
+            text="Login",
+            command=lambda: self.try_login(controller)
+        ).pack(pady=20)
+
+        self.message = tk.Label(self, text="", fg="red")
+        self.message.pack()
+
+    def try_login(self, controller):
+        user = self.username.get()
+        pwd = self.password.get()
+
+        # temp login logic
+        # replace with envvars + database hashed query
+        if user == "root" and pwd == "toor":
+            controller.create_navbar()
+            controller.show_frame(HomePage)
+        else:
+            self.message.config(text="Invalid credentials")
+
+class HomePage(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        #  instatiate the frame as you would instantiate a normal frame
+        tk.Label(self, text="Welcome!", font=("Arial", 18)).pack(pady=20)
+
+
+class overDueBalancePage(tk.Frame):
+    def __init__(self,parent,controller):
+        super().__init__(parent)
+        tk.Label(self, text="OverDue Balances", font=("Arial", 18)).pack(pady=20)
+
+        columns = ("Name", "Amount Unpaid", "Phone Number", "Email")
+
+        table = ttk.Treeview(self, columns=columns, show="headings")
+        table.pack(fill="both", expand=True)
+
+        for col in columns:
+            table.heading(col, text=col)
+
+        table.insert("", "end", values=("John Smith", 25.00, "07584710472","johnSmith@yahoo.com"))
+        table.insert("", "end", values=("Test 1", 25.60, "0988865239", "test2@mail.com"))
+        table.insert("", "end", values=("Test 2", 400.00, "06876435670","test2@mail.com"))
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
